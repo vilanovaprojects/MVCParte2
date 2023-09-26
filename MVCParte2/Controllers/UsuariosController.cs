025264b2e858eb3869b67be925301dcfc47d63ce
vilanovaprojects <mcvmoises@gmail.com>
2023-09-26 03:30:26 +0200

    primer commit

---
+++ MVCParte2/Controllers/UsuariosController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCParte2.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListaUsuarios()
+        {
+            List<string> listaUsuarios = new List<string>();
+            listaUsuarios.Add("Fernando");
+            listaUsuarios.Add("Alejandra");
+            listaUsuarios.Add("Raul");
+            listaUsuarios.Add("Martín");
+            listaUsuarios.Add("Ernesto");
+
+            //ViewBag.ListaUsuarios = listaUsuarios;
+            //TempData["ListaUsuarios"] = listaUsuarios;
+            //Session["ListaUsuarios"] = listaUsuarios;
+            ViewData["ListaUsuarios"] = listaUsuarios;
+
+            return View(listaUsuarios);
+        }
+
+        [HttpPost]
+        public ActionResult ListaUsuarios(string selUsuarios)
+        {
+            ViewBag.Nombre = selUsuarios;
+            //List<string> listaUsuarios = ViewBag.ListaUsuarios;
+            //List<string> listaUsuarios = (List<string>)TempData["ListaUsuarios"];
+            //List<string> listaUsuarios = (List<string>)Session["ListaUsuarios"];
+            List<string> listaUsuarios = (List<string>)ViewData["ListaUsuarios"];
+            return View(listaUsuarios);
+        }
+    }
+}
